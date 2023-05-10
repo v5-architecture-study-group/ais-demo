afc8f4997ea84ebc4d95f2b95e6de21136a633b7
Petter Holmström <petter@vaadin.com>
2023-05-10 13:12:41 +0300

    Initial import

---
+++ src/main/java/com/example/demo/ais/AISDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo.ais;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AISDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AISDemoApplication.class, args);
+	}
+
+}
